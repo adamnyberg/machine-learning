df47d9b99d38abedbf6a51f510fa8f7c258e187c
Rasmus Johns <Rasmus Johns>
2016-11-08 15:30:20 +0100

    perfect assignment 1 score 5/7 perfect score

--- Rasmus/Assignment1/lab1.R
+++ Rasmus/Assignment1/lab1.R
@@ -1,28 +1,5 @@
 library(kknn)
 
-setwd("C:/Users/rasmu/Documents/Code/tdde01/tdde01/Rasmus/Assignment1/")
-
-data <- read.csv("spambase.csv")
-n=dim(data)[1]
-set.seed(12345) 
-id=sample(1:n, floor(n*0.5)) 
-train=data[id,] 
-test=data[-id,]
-
-
-## oklartheter##
-##model = train.kknn( Spam~. , data = data, train = train, test = test, kmax = 9)
-##kknn(formula = formula(train), train, test, k=5)
-
-##model_preds = predict(model, test[, 49])
-##confusion_mat = table(test[,49], model_preds)
-###
-
-spam_train = train$Spam
-spam_test = test$Spam
-train$Spam = NULL;
-test$Spam = NULL;
-
 c_func=function(X,Y){
   #X=train, Y=test
   x_part = as.matrix(X)/sqrt(rowSums(X^2))
@@ -60,18 +37,16 @@ knearest=function(data,K,newdata){
   return(result)
 }
 
-confusion_matrix=function(data, K, newdata) {
-  pred_prob = knearest(data, K, newdata)
+confusion_matrix=function(pred_prob) {
   pred_prob = replace(pred_prob, pred_prob < 0.5, 0)
   pred_prob = replace(pred_prob, pred_prob >= 0.5, 1)
   
-  conf_mat = table(pred_prob == 1, spam_test == 1)
+  conf_mat = table(pred_prob, spam_test)
   
   return(conf_mat)
 }
 
-misclass_rate=function(data, K, newdata) {
-  pred_prob = knearest(data, K, newdata)
+misclass_rate=function(pred_prob) {
   pred_prob = replace(pred_prob, pred_prob < 0.5, 0)
   pred_prob = replace(pred_prob, pred_prob >= 0.5, 1)
   
@@ -84,4 +59,41 @@ misclass_rate=function(data, K, newdata) {
   return(mis_class_count/n)
 }
 
+plot_ROC=function(pred_prob) {
+  
+  TPR = c()
+  FPR = c()
+  
+  for (i in seq(0.05, 0.95, 0.05)) {
+    temp_prob = pred_prob
+    temp_prob = replace(temp_prob, temp_prob < i, 0)
+    temp_prob = replace(temp_prob, temp_prob >= i, 1)
+    
+    conf_mat = table(temp_prob, spam_test)
+    index = i*20
+    TPR[index] = conf_mat[1,1]/sum(conf_mat[1,])
+    FPR[index] = conf_mat[2,1]/sum(conf_mat[2,])
+  }
+  plot(FPR, TPR)
+}
+
+setwd("C:/Users/rasmu/Documents/Code/tdde01/tdde01/Rasmus/Assignment1/")
+
+data <- read.csv("spambase.csv")
+n=dim(data)[1]
+set.seed(12345) 
+id=sample(1:n, floor(n*0.5)) 
+train=data[id,] 
+test=data[-id,]
+
+model = train.kknn( Spam~. , data = train, kmax = 5)
+
+spam_train = train$Spam
+spam_test = test$Spam
+train$Spam = NULL;
+test$Spam = NULL;
+
+model_preds = predict(model, test)
+confusion_matrix(model_preds)
+misclass_rate(model_preds)
 
